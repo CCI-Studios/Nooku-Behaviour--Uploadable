115aad8aa9c6c06fa79dbfad72935d12cb5d82fc
Jonathan Bennett <jbennett@ccistudios.com>
2010-12-28 10:45:40 -0500

    Testing different thumbnail options

--- administrator/components/com_test/databases/tables/items.php
+++ administrator/components/com_test/databases/tables/items.php
@@ -4,7 +4,13 @@ defined('KOOWA') or die;
 class ComTestDatabaseTableItems extends KDatabaseTableDefault {
 	
 	protected function _initialize(KConfig $config) {
-		$config->behaviors = array('com.test.database.behavior.uploadable');
+		$uploadable	= KFactory::get('com.test.database.behavior.uploadable', array(
+			'thumbs' => array(
+				array('prefix' => 'large', 'suffix'=> '', 'width'=>420, 'height'=>450),
+				array('prefix' => 'thumb', 'suffix'=> '', 'width'=>100, 'height'=>100),
+			),
+		));
+		$config->behaviors = array($uploadable);
 		parent::_initialize($config);
 	}
 }
